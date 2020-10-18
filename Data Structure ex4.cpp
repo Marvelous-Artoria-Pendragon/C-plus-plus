32efe65857160ebe2b28ae87d434755f347049d6
Marvelous-Pendragon-Artoria <1542752488@qq.com>
2020-10-18 14:00:40 +0800

    assigment 4-5

--- Data Structure ex4.cpp
+++ Data Structure ex4.cpp
@@ -17,29 +17,28 @@ class CircList
     public:
         CircList<T>();                                      //构造函数
         ~CircList<T>();                                     //析构函数
-        CircList<T>(const CircList<T> &);                   //拷贝构造函数
-        void reverse();                     //将链表元素逆转
+        CircList<T>(CircList<T> &);                   //拷贝构造函数
+        void reverse();                                     //将链表元素逆转
+        LinkNode<T> *getRear() {return rear;}               //返回尾指针
         template <class Y>
         friend ostream &operator << (ostream &, const CircList<Y> &);        //重载输出流函数
 
     protected:
         LinkNode<T> *rear;      //尾指针
-        int length;
 };
 
 template <class T>
 CircList<T>::CircList()
 {
     int i = 0;
-    rear = new LinkNode<T>(i);         //初始化一个表头结点
-    if (rear == NULL) {cerr << "存储分配错误！" << endl; exit(1);}          //动态分配失败
-    rear->link = rear;              //表头结点指向自己
-    while (i < 8)                 //创建7个结点
+    rear = new LinkNode<T>(i);                                              //初始化一个表头结点
+    if (rear == NULL) {cerr << "存储分配错误！" << endl; exit(1);}           //动态分配失败
+    rear->link = rear;                                                      //表头结点指向自己
+    while (i++ < 4)                                                           //创建7个结点
     {
-        i++;
         LinkNode<T> *p = new LinkNode<T>(i);
         if (p == NULL) {cerr << "存储分配错误！" << endl; exit(1);}          //动态分配失败
-        p->link = rear->link;
+        p->link = rear->link;                                               //尾插入
         rear->link = p;
         rear = p;
     }
@@ -48,29 +47,31 @@ CircList<T>::CircList()
 template <class T>
 CircList<T>::~CircList()
 {
-    LinkNode<T> *del, *current = rear;
-    do
+    LinkNode<T> *del;
+    while (rear->link != rear)
     {
-        del = current;
-        current = current->link;
+        del = rear->link;
+        rear->link = del->link;
         delete del;
-    } while (current != rear);
+    }
+    delete rear;
 }
 
 template <class T>
-CircList<T>::CircList(const CircList<T> &L)
+CircList<T>::CircList(CircList<T> &L)
 {
-    LinkNode<T> *current = L->rear;
-    rear = new LinkNode<T>(current->data);
+    LinkNode<T> *srcptr = L.getRear()->link;
+    rear = new LinkNode<T>(0);
     if (rear == NULL) {cerr << "存储分配错误！" << endl; exit(1);}          //动态分配失败
     rear->link = rear;          //头尾链接
 
-    while (current->link != L->rear)
+    while (srcptr->link != L.getRear()->link)
     {
-        LinkNode<T> *p = new LinkNode<T>(current->data);
-        p->link = rear->link;
-        rear = p;
-        current = current->link;
+        LinkNode<T> *newNode = new LinkNode<T>(srcptr->link->data);
+        newNode->link = rear->link;
+        rear->link = newNode;
+        srcptr = srcptr->link;          //复制的下一个结点
+        rear = newNode;
     }
 }
 
@@ -79,15 +80,15 @@ template <class T>
 void CircList<T>::reverse()
 {
     LinkNode<T> *next, *current, *last, *ret;       //ret记录逆转后的链表的最后一个结点， next指向下一个结点
-                                            //current指向当前结点，last指向上一个结点
-    last = current = rear, ret = next = current->link;
+                                                    //current指向当前结点，last指向上一个结点
+    last = rear; current = last->link; ret = next = current->link;
     do
     {
-        current = next;             //移动到下一个结点
-        next = current->link;       //将下一个结点指向当前结点
-        current->link = last;       //将当前结点指向上一个结点
-        last = current;             //当前结点成为下一次修改过程的上一个结点
-    } while (current != rear);
+        current->link = last;
+        last = current;
+        current = next;
+        next = next->link;
+    } while (last != rear);
     rear = ret;
 }
 
@@ -112,20 +113,22 @@ template <class T>
 ostream &operator << (ostream &output, const CircList<T> &c)
 {
     LinkNode<T> *temp = c.rear->link;
-    do
+    while(temp->link != c.rear->link)
     {
-        cout << temp->data << ' ';
+        cout << temp->link->data << ' ';
         temp = temp->link;
-    } while (temp != c.rear->link);
+    }
     return output;
 }
 
 int main()
 {
-    CircList<int> cl;
-    cout << cl << endl;
-    cl.reverse();
-    cout << cl << endl;
+    CircList<int> c1;
+    cout << c1 << endl;
+    c1.reverse();
+    cout << c1 << endl;
+    CircList<int> c2 = c1;
+    cout << c2 << endl;
     system("pause");
     return 0;
 }
--- Data Structure ex5.cpp
+++ Data Structure ex5.cpp
@@ -28,81 +28,78 @@ class Calculator
 {
     //对从键盘读入的后缀表达式求值
     public:
-        //Calculator(int sz):s(sz){}                          构造函数
-        void Run();                                         //执行表达式计算
-        void Clear();
+        Calculator() {infix = ""; operand_arr = new char[20];}            //构造函数,初始化数
+        ~Calculator() {delete operand_arr;}
+        double Run();                                                       //执行表达式计算
+        void Clear();                                                     //清空操作数栈和操作符数组
+        void readinfix(string str) {infix = str;}                         //读取中缀表达式
     private:
-        SeqStack<double> s;                                 //存储操作数的栈
-        string infix;                                       //中缀表达式
-        string postfix;                                     //后缀表达式
-        void AddOperand(double value);                      //进操作数栈
-        bool get2Operand(double &left, double &right);      //从栈中退出两个操作数
-        void DoOperator(char op);                           //形成运算指令， 进行计算
-        void toPostfix();                                   //将中缀表达式转换为后缀表达式
-
+        void toPostfix();                                                 //将中缀表达式转换为后缀表达式
+        SeqStack<double> number;                                          //存储操作数的栈
+        string infix;                                                     //中缀表达式
+        char *operand_arr;                                                //存储操作符的数组
+        bool get2Operand(double num[]);                                   //从栈中退出两个操作数
+        void DoOperator(char op);                                         //形成运算指令，对一个操作符进行计算
 };
 
-//进行运算
+//对一个操作符进行运算
 void Calculator::DoOperator(char op)
 {
-    double left, right, value; int result;
-    result = get2Operand(left, right);
+    double num[2]; int result;                                          //num[0]表示左操作数，num[1]为右操作数
+    result = get2Operand(num);                                          //判断是否获取成功
     if (result)
         switch(op)
         {
-            case '+': value = left + right; s.Push(value); break;
-            case '-': value = left - right; s.Push(value); break;
-            case '*': value = left * right; s.Push(value); break;
-            case '/': if (right == 0.0)
+            case '+': number.Push(num[0] + num[1]); break;
+            case '-': number.Push(num[0] - num[1]); break;
+            case '*': number.Push(num[0] * num[1]); break;
+            case '/': if (num[1] == 0.0)
                       {cerr << "Divide by 0!" << endl; Clear();}
-                      else {value = left / right; s.Push(value);}
+                      else number.Push(num[0] / num[1]);
                       break;
         }
     else Clear();
 }
 
 //从栈里取两个操作数
-bool Calculator::get2Operand(double &left, double &right)
+bool Calculator::get2Operand(double num[])
 {
-    if (s.IsEmpty()) {cerr << "缺少右操作数！" << endl; return false;}
-    s.Pop(right);           //先提取右操作数
-    if (s.IsEmpty()) {cerr << "缺少左操作数！" << endl; return false;}
-    s.Pop(left);            //再提取左操作数
+    if (number.IsEmpty()) {cerr << "缺少右操作数！" << endl; return false;}
+    number.Pop(num[1]);            //先提取右操作数
+    if (number.IsEmpty()) {cerr << "缺少左操作数！" << endl; return false;}
+    number.Pop(num[0]);            //再提取左操作数
     return true;
 };
 
-//将操作数压入栈中
-void Calculator::AddOperand(double value)
-{s.Push(value);}
-
 //清空栈所有的数据
 void Calculator::Clear()
-{s.makeEmpty();}
-
-void Calculator::Run()
 {
-    char ch; double newOperand, result;
-    int i = 0;
-    while (ch = postfix[i++], ch != '#')
-        switch(ch)
-        {
-            case '+': case '-': case '*': case '/': DoOperator(ch); break;      //判断是操作符，执行计算
-            default: cin.putback(ch);               //将字符放回输入流
-                     cin >> newOperand;             //重新读操作数
-                     AddOperand(newOperand);        //将操作数放入栈中
-        }
-    s.Pop(result);
-    cout << "\n" << result << endl;
+    number.makeEmpty();
+    memset(operand_arr, 0, 20);
 }
 
+//对表达式进行运算
+double Calculator::Run()
+{
+    toPostfix();
+    char op; double result; int i = 0;
+    while (op = operand_arr[i++], op != '#')
+        switch(op)
+        {
+            case '+': case '-': case '*': case '/': DoOperator(op); break;      //判断是操作符，执行计算
+            default: {cerr << "非合法操作符！" << endl; exit(1);}
+        }
+    number.Pop(result);
+    Clear(); return result;
+}
 
 //栈内(in stack priority)优先数
 int isp(char ch)
 {
     switch(ch)
     {
-        case '#' : return 0;
-        case '(' : return 1;
+        case '#': return 0;
+        case '(': return 1;
         case '*': case '/': case '%': return 5;
         case '+': case '-': return 3;
         case ')': return 6;
@@ -116,7 +113,7 @@ int icp(char ch)
     switch(ch)
     {
         case '#': case '=': return 0;
-        case '(' : return 6;
+        case '(': return 6;
         case '*': case '/': case '%': return 4;
         case '+': case '-': return 2;
         case ')': return 1;
@@ -128,23 +125,23 @@ int icp(char ch)
 void Calculator::toPostfix()
 {
     SeqStack<char> operand;                                     //存储操作符的栈
-    string postfix[50];                                         //返回的后缀表达式字符数组
     char read = '#', top, op;                                   //read为当前读取的字符，top为栈顶字符，op为操作符
-    int i = 0, j = 0, k = 0;                                //i用于遍历中缀表达式每个字符，j表示后缀表达式的第几个字符
-    regex pn("\\d+.?\\d*");                                     //匹配正数的正则表达式
+    int i = 0, j = 0;                                           //i用于遍历中缀表达式每个字符，j表示操作符序号
+    regex pn("-?\\d+\\.?\\d*");                                 //匹配正数的正则表达式
     smatch result;                                              //保存匹配结果的实例对象
     string::const_iterator iterstart = infix.begin();           //迭代器开始部分
     string::const_iterator iterend = infix.end();               //迭代器结束部分
 
-    operand.Push(read); read = infix[i++];                      //栈底放一个“#”， 读入一个字符
-    while (!s.IsEmpty() && read != '\0')                        //连续处理，栈不为空且中缀表达式未读完
+    operand.Push(read); read = infix[i];                        //栈底放一个“#”， 读入一个字符
+    while (!operand.IsEmpty() && read != '\0')                  //连续处理，栈不为空且中缀表达式未读完
         if (isdigit(read))                                      //当前读到的字符是数字
         {
             if (regex_search(iterstart, iterend, result, pn))   //正则表达式匹配这个数字
             {
-                postfix[k++] = result[0];
-                iterstart = result[0].second;
-                read = infix[i++];
+                number.Push(stof(result[0].str()));             //将操作数压入操作数栈中
+                iterstart = result[0].second;                   //下一次从匹配到字符串的末位置开始匹配
+                i += result.length();
+                read = infix[i];                                //继续读取字符
             }
             else {cerr << "未匹配到操作数!"; exit(1);}
         }
@@ -153,34 +150,52 @@ void Calculator::toPostfix()
             operand.getTop(top);                                //取栈顶操作符
             if (isp(top) < icp(read))                           //新输入操作符read优先级高
             {
-                operand.Push(read);            //进栈
-                if (read == '(' && !isdigit(infix[i + 1]))           //判断括号后跟的不是数字，默认判断为负数
+                operand.Push(read);                             //进栈
+                if (read == '(')
                 {
-                    if (regex_search(iterstart, iterend, result, pn))
+                    if (!isdigit(infix[i + 1]))                 //判断括号后跟的不是数字，默认判断为负数
                     {
-                        postfix[k++] = result[0];
-                        iterstart = result[0].second;
+                        if (regex_search(iterstart, iterend, result, pn))
+                        {
+                            number.Push(stof(result[0].str()));
+                            iterstart = result[0].second;
+                            i += result[0].length();
+                        }
+                        else {cerr << "未匹配到操作数!"; exit(1);}
                     }
-                    else {cerr << "未匹配到操作数!"; exit(1);}
+                    else {cerr << "括号包括非合法操作符!" << endl; exit(1);}
                 }
-                read = infix[i++];            //读入下一个字符
+                read = infix[++i];                              //读入下一个字符
             }
-            else if(isp(top) > icp(read))              //新输入操作符ch优先级高
-                {operand.Pop(op); cout << op;}      //退栈，并输出
+            else if(isp(top) > icp(read))                       //新输入操作符read优先级低
+                {   
+                    operand.Pop(op);                            //操作符退栈
+                    operand_arr[j++] = op;                      //将操作符放入operand_arr数组中
+                }
             else
             {
-                operand.Pop(op);                      //输入操作符优先级等于栈顶优先级
-                if (op == '(') read = infix[i++];
+                operand.Pop(op);                                //输入操作符优先级等于栈顶优先级(取出配对的括号)
+                if (op == '(') read = infix[++i];
             }
         }
+    operand_arr[j] = '#';                                       //添加一个'#'作为结束标记
 }
 
 int main()
 {
-    string infix;
-    cout << infix;
-    /*Calculator cl;
-    cl.Run();*/
+    Calculator cl;
+    string infix; int n;
+    cout << "请输入需要计算的表达式数量："; cin >> n;
+    double *result = new double[n];
+    cout << "请依次输入表达式：" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cin >> infix;
+        cl.readinfix(infix + "=");
+        result[i] = cl.Run();
+    }
+    for (int i = 0; i < n; i++)
+    cout << result[i] << endl;
     system("pause");
     return 0;
 }
