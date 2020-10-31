d2b06ed0dded4f7936c234d1cbc5a0627e9467f1
Marvelous-Pendragon-Artoria <1542752488@qq.com>
2020-10-31 13:39:26 +0800

    Stack

--- Stack.h
+++ Stack.h
@@ -14,7 +14,7 @@ class Stack
         Stack(){};  //构造函数
         virtual void Push(const T &x) = 0;      //新元素x进栈
         virtual bool Pop(T &x) = 0;             //栈顶元素出栈，由x返回
-        virtual bool getTop(T &x) const = 0;     //读取栈顶元素，由x返回
+        virtual bool getTop(T &x) const = 0;    //读取栈顶元素，由x返回
         virtual bool IsEmpty() const = 0;       //判断栈是否为空
         virtual bool IsFull() const = 0;        //判断栈是否为满
         virtual int getSize() const = 0;        //计算栈中元素个数
