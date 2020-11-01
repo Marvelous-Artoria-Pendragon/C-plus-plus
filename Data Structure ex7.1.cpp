6fe02eea9abbab46bc3829a3438317a640bb5721
Marvelous-Pendragon-Artoria <1542752488@qq.com>
2020-11-01 17:41:39 +0800

    assignment 7

--- Data Structure ex7.1.cpp
+++ Data Structure ex7.1.cpp
@@ -2,10 +2,7 @@
 1.编写一个将二叉链式结构存储的二叉树的所有叶子结点从左向右链接成单链表的算法。
 */
 
-#include <iostream>
 #include "BinaryTree.h"
-using namespace std;
-
 int main()
 {
     BinaryTree<char> BT('.');
@@ -13,18 +10,16 @@ int main()
     cin >> BT;  //ABC..DE.G..F...
     cout << "树的前序表达式为：" << endl << BT;
     BinTreeNode<char> *root = BT.getRoot();
-    LinkNode<char> *l = new LinkNode<char>;             //链表首地址(带表头)
-    LinkNode<char> *current = l;      //用于遍历操作
+    LinkNode<char> *l = new LinkNode<char>, *current = l;             //链表首地址(带表头)
     BT.leafLinkedList(root, current);
     current = l;
-    while (current != NULL)
+    while (current != NULL)                     //循环输出叶子链表
     {
         cout << current->data << " ";
         current = current->link;
     }
-
     LinkNode<char> *del;
-    while (l != NULL)
+    while (l != NULL)                           //循环删除叶子链表
     {
         del = l;
         l = l->link;
---
+++ Data Structure ex7.2.cpp
@@ -0,0 +1,24 @@
+/*
+设二叉树采用二叉链表表示，指针root指向根结点，试编写一个在二叉树中查找值为x的结点，并打印该结点所有祖先结点的算法。
+在此算法中，假设值为x的结点不多于一个
+*/
+#include "BinaryTree.h"
+int main()
+{
+    BinaryTree<char> BT('.');
+    cout << "请输入前序表达式(空结点用'.'表示)：";
+    cin >> BT;  //ABC..DE.G..F...
+    cout << "树的前序表达式为：" << endl << BT;
+    SeqStack<BinTreeNode<char> * > s;                               //用于存储所有祖先结点
+    BinTreeNode<char> *root = BT.getRoot();
+    BT.findAncestors(root, 'E', s);
+    cout << "该结点的所有祖先结点的值为：" << endl;
+    while (!s.isEmpty())            //循环输出储存的结点的值
+    {
+        BinTreeNode<char> *temp = NULL;
+        s.pop(temp);
+        cout << temp->data << " ";
+    }
+    system("pause");
+    return 0;
+}
