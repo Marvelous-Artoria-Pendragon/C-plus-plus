a2abf560fc3acca34b6cb6c3302c31c55d5e10f1
Marvelous-Pendragon-Artoria <1542752488@qq.com>
2020-11-04 19:22:12 +0800

    assignment 8

--- Data Structure ex8.1.cpp
+++ Data Structure ex8.1.cpp
@@ -1,26 +1,58 @@
 /*一棵具有 n 个结点的完全二叉树存放在二叉树的顺序存储结构中，试编写非递归算法对该树进行中序遍历*/
 #include "BinaryTree.h"
-using namespace std;
-
+#include <math.h>
+/*
 template <class T>
-class LinearBinaryTree
+LinearBinTreeNode<T> * createCompleteLinearBinaryTree(int n)
 {
-    private:
-        LinearBinTreeNode<T> *root;                                             //根节点
-    public:
-        LinearBinaryTree(): root(NULL){}                                        //构造函数，根节点为空
-        LinearBinaryTree(LinearBinaryTree<T> x[]);                              //使用已有二叉树数组构造
-        ~LinearBinaryTree() {delete[] root;}                                    //析构函数
-        void preOrder(int subTree);                                             //非递归算法对subTree为根的树进行中序遍历
-};
 
-template <class T>
-void LinearBinaryTree<T>::preOrder(int subTree)
-{
-    SeqStack<int> s;
-    s.push(subTree);
-    while (!s.isEmpty())
+    LinearBinTreeNode<T> *arr = new LinearBinTreeNode[n];
+    int parent = -1, t = pow(2, n - 1);
+    for (int i = 0; i < n; i++)
     {
-
+        cin >> arr[i].data;
+        arr[i].parent = (i - 1) / 2;
+        if (i < t);                                 //最后一层结点无子女结点
+        {
+            arr[i].leftChild = 2 * i + 1;
+            arr[i].rightChild = 2 * i + 2;
+        }
     }
+    return arr;
+}
+*/
+
+/*
+//对二叉树的顺序存储结构，使用非递归方式实现中序遍历
+template <class T>
+void preOrder(LinearBinTreeNode<T> *arr, void (*visit)(LinearBinTreeNode<T> &))
+{
+    SeqStack<int> s; int p = 0;                              //p为遍历指针，从根结点序号开始
+    do
+    {
+        while (p > -1)                                       //指针未进入到最左下的结点，不空
+        {
+            s.push(p);                                       //该子树沿途结点进栈
+            p = arr[p].leftChild;                            //遍历指针进到到左子女结点
+        }
+        if (!s.isEmpty())
+        {
+            s.pop(p); visit(arr[p]);                         //退出一个结点，访问根结点
+            p = arr[p].rightChild;                           //进入右子女结点
+        }
+    } while (!s.isEmpty());
+}
+*/
+template <class T>
+void display(LinearBinTreeNode<T> &current) {cout << current.data << " ";}
+
+int main()
+{
+    cout << "请输入结点个数：";
+    int n; cin >> n;    //ABCDEFGHIJ
+    LinearBinTreeNode<char> *arr = createCompleteLinearBinaryTree<char>(n);
+    preOrder<char>(arr, display<char>);
+    delete[] arr;
+    system("pause");
+    return 0;
 }
--- Data Structure ex8.2.cpp
+++ Data Structure ex8.2.cpp
@@ -2,6 +2,41 @@
 
 #include "BinaryTree.h"
 
+/*
+//将二叉链式结构转换成顺序存储结构，函数返回数组首地址;
+//index标记当前数组第几个元素
+template <class T>
+LinearBinTreeNode<T> *BinaryTree<T>::toLinearBinaryTree(BinTreeNode<T> *current) const
+{
+    if (current == NULL) return NULL;                                               //根结点为空，返回空
+    LinkedQueue<BinTreeNode<T> *> traverse;                                         //层次遍历每个结点队列
+    LinkedQueue<int> parent;                                                        //存放父结点的队列
+    int count = 0, currentindex = 0, temp = 0, size = Size();                       //count统计结点数，currentindex为当前处理的结点
+    LinearBinTreeNode<T> *arr = new LinearBinTreeNode<T>[size]();
+    traverse.EnQueue(current);                                                      //从根结点开始队列处理
+    parent.EnQueue(-1);                                                             //根结点无父节点，-1进队
+    while (!traverse.isEmpty())                                                     //队列不为空
+    {
+        traverse.DeQueue(current); parent.DeQueue(temp);                            //退出一个结点，
+        arr[currentindex].data = current->data; arr[currentindex].parent = temp;
+        if (current->leftChild != NULL) 
+        {
+            traverse.EnQueue(current->leftChild);                                   //左子女进队
+            arr[currentindex].leftChild = ++count;                                  //父节点的左子女索引
+            parent.EnQueue(currentindex);                                           //该结点成为其子女的父节点
+        }
+        if (current->rightChild != NULL)
+        {
+            traverse.EnQueue(current->rightChild);                                  //右子女进队
+            arr[currentindex].rightChild = ++count;                                 //父节点的右子女索引
+            parent.EnQueue(currentindex);                                           //该结点成为其子女的父节点
+        }
+        currentindex++;
+    }
+    return arr;
+}
+*/
+
 int main()
 {
     BinaryTree<char> BT('.');
@@ -13,9 +48,7 @@ int main()
     int n = 9;
     cout << "data " << "parent " << "leftChild " << "rightChild " << endl;
     for (int i = 0; i < n; i++)
-    {
         cout << L[i].data << "\t" << L[i].parent << "\t" << L[i].leftChild << "\t" << L[i].rightChild << endl;
-    }
     system("pause");
     return 0;
 }
